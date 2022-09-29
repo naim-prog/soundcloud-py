389339bec6b1dfdd655b958220ee1043fc61de40
Naím Rodríguez <naimrodrey@protonmail.com>
2022-09-29 13:48:28 +0200

    Added new features
    -Get comments from a track
    -Get users profiles that like a track

--- soundcloudpy.py
+++ soundcloudpy.py
@@ -18,6 +18,8 @@ class Soundcloud:
         self.headers = {"Authorization" : o_auth, "Accept": "application/json",
                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0"}
 
+    # ---------------- USER ----------------
+
     def get_account_details(self):
         req = requests.get("https://api-v2.soundcloud.com/me", headers=self.headers)
         return req.text
@@ -29,12 +31,30 @@ class Soundcloud:
         req = requests.get(f"https://api-v2.soundcloud.com/users/{user_id}?client_id={self.client_id}", headers=self.headers)
         return req.text
 
+    # ---------------- TRACKS ----------------
+
     def get_last_track_info(self, limit=1):
         """
         :param limit: number of last tracks reproduced
         """
         req = requests.get(f"https://api-v2.soundcloud.com/me/play-history/tracks?client_id={self.client_id}&limit={limit}", headers=self.headers)
         return req.text
+
+    def get_track_likes_users(self, track_id):
+        """
+        :param track_id: track id 
+        """
+        req = requests.get(f"https://api-v2.soundcloud.com/tracks/{track_id}/likers?client_id={self.client_id}", headers=self.headers)
+        return req.text
+
+    def get_track_details(self, track_id):
+        """
+        :param track_id: track id 
+        """
+        req = requests.get(f"https://api-v2.soundcloud.com/tracks?ids={track_id}&client_id={self.client_id}", headers=self.headers)
+        return req.text
+
+    # ---------------- PLAYLISTS ----------------  
 
     def get_account_playlists(self):
         req = requests.get(f"https://api-v2.soundcloud.com/me/library/all?client_id{self.client_id}", headers=self.headers)
@@ -47,11 +67,13 @@ class Soundcloud:
         req = requests.get(f"https://api-v2.soundcloud.com/playlists/{playlist_id}?representation=full&client_id={self.client_id}", headers=self.headers)
         return req.text
 
-    def get_track_details(self, track_id):
+    # ---------------- MISCELLANEOUS ----------------
+
+    def get_recommended(self, track_id):
         """
-        :param track_id: track id 
+        :param track_id: track id to get recommended tracks from this
         """
-        req = requests.get(f"https://api-v2.soundcloud.com/tracks?ids={track_id}&client_id={self.client_id}", headers=self.headers)
+        req = requests.get(f"https://api-v2.soundcloud.com/tracks/{track_id}/related?client_id={self.client_id}", headers=self.headers)
         return req.text
 
     def get_stream_url(self, track_id):
@@ -68,9 +90,12 @@ class Soundcloud:
 
         return dict(json.loads(req.text)).get('url')
 
-    def get_recommended(self, track_id):
+    def get_comments_track(self, track_id, limit=100):
         """
-        :param track_id: track id to get recommended tracks from this
+        :param track_id: track id for get the comments from
+        :param limit:    limit of tracks to get
+
+        :add: with the "next_href" in the return json you can keep getting more comments than the limit
         """
-        req = requests.get(f"https://api-v2.soundcloud.com/tracks/{track_id}/related?client_id={self.client_id}", headers=self.headers)
+        req = requests.get(f"https://api-v2.soundcloud.com/tracks/{track_id}/comments?threaded=0&filter_replies=1&client_id={self.client_id}&limit={limit}&offset=0&linked_partitioning=1&app_version=1664196962", headers=self.headers)
         return req.text
